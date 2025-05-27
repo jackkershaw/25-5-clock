c80e417f47be68c2c5299e5cfeb56d929d9c9d57
jackkershaw <hello@jackkershaw.net>
2025-05-27 22:48:44 +0100

    passing user stories

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,82 @@ import { useState } from "react";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState(25);
+  const [timerOn, setTimerOn] = useState(false);
+
+  const reset = () => {
+    setTimerOn(false);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLeft(25);
+  };
+
+  const start;
 
   return (
     <div className="flex flex-col items-center justify-center p-10">
       <h1>25+5 Clock</h1>
       <div>
         <h2 id="break-label">Break length</h2>
-        <button id="break-decrement">-</button>
-        <button id="break-increment">+</button>
+        <p id="break-length">{breakLength}</p>
+        <button
+          id="break-decrement"
+          onClick={() => {
+            if (breakLength > 0) {
+              setBreakLength(breakLength - 1);
+            }
+          }}
+        >
+          -
+        </button>
+        <button
+          id="break-increment"
+          onClick={() => {
+            if (breakLength < 60) {
+              setBreakLength(breakLength + 1);
+            }
+          }}
+        >
+          +
+        </button>
       </div>
       <div>
         <h2 id="session-label">Session length</h2>
-        <button id="session-decrement">-</button>
-        <button id="session-increment">+</button>
+        <p id="session-length">{sessionLength}</p>
+        <button
+          id="session-decrement"
+          onClick={() => {
+            if (sessionLength > 0) {
+              setSessionLength(sessionLength - 1);
+            }
+          }}
+        >
+          -
+        </button>
+        <button
+          id="session-increment"
+          onClick={() => {
+            if (sessionLength < 60) {
+              setSessionLength(sessionLength + 1);
+            }
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <h2 id="timer-label">Session</h2>
+        <p id="time-left">
+          {new Intl.DateTimeFormat("en-GB", {
+            minute: "numeric",
+            second: "numeric",
+          }).format(timeLeft * 60 * 1000)}
+        </p>
+        <button id="start_stop">[]</button>
+        <button id="reset" onClick={reset}>
+          X
+        </button>
       </div>
     </div>
   );
