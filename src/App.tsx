57b859a4548b9fdce120d5ff27fb2a6ae1351f34
jackkershaw <hello@jackkershaw.net>
2025-05-27 21:59:44 +0100

    fulfilling user stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+
   return (
     <div className="flex flex-col items-center justify-center p-10">
       <h1>25+5 Clock</h1>
+      <div>
+        <h2 id="break-label">Break length</h2>
+        <button id="break-decrement">-</button>
+        <button id="break-increment">+</button>
+      </div>
+      <div>
+        <h2 id="session-label">Session length</h2>
+        <button id="session-decrement">-</button>
+        <button id="session-increment">+</button>
+      </div>
     </div>
   );
 }
