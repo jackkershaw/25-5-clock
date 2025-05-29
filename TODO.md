4d8b4b58ec82d342b70f7f6bebd17ca4a54bb718
jackkershaw <hello@jackkershaw.net>
2025-05-29 22:46:10 +0100

    passing tests adding sounds

--- TODO.md
+++ TODO.md
@@ -1,13 +1,3 @@
-- Set date properly in MM:SS format not international date format - this will just need to be total time in seconds
-
-User Story #22: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a break has begun.
-
-User Story #23: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), a new break countdown should begin, counting down from the value currently displayed in the id="break-length" element.
-
-User Story #24: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a session has begun.
-
-User Story #25: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), a new session countdown should begin, counting down from the value currently displayed in the id="session-length" element.
-
 User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audio tag and have a corresponding id="beep".
 
 User Story #27: The audio element with id="beep" must be 1 second or longer.
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
-  const [timeLeft, setTimeLeft] = useState(25);
+  const [breakLength, setBreakLength] = useState(5 * 60);
+  const [sessionLength, setSessionLength] = useState(25 * 60);
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [timerOn, setTimerOn] = useState(false);
+  const [breakOn, setBreakOn] = useState(false);
+  const [soundPlaying, setSoundPlaying] = useState(false);
 
   const reset = () => {
-    setBreakLength(5);
-    setSessionLength(25);
-    setTimeLeft(25);
+    setBreakLength(5 * 60);
+    setSessionLength(25 * 60);
+    setTimeLeft(25 * 60);
     setTimerOn(false);
+    setSoundPlaying(false);
   };
 
   const startStop = () => {
@@ -22,18 +25,46 @@ function App() {
   };
 
   const countDown = () => {
-    setTimeLeft((timeLeft) => timeLeft - 0.01666667);
+    setTimeLeft((timeLeft) => timeLeft - 1);
   };
 
   useEffect(() => {
-    if (!timerOn) return;
+    if (soundPlaying) {
+      const audioElement = document.getElementById(
+        "beep"
+      ) as HTMLAudioElement;
+      audioElement.play();
+    }
+  });
+
+  useEffect(() => {
+    if (!timerOn && !breakOn) return;
     const interval = setInterval(() => {
       if (timeLeft > 0) {
         countDown();
+      } else if (timeLeft == 0) {
+        if (timerOn) {
+          setSoundPlaying(true);
+          setBreakOn(true);
+          setTimeLeft(breakLength);
+          setTimerOn(false);
+        } else if (breakOn) {
+          setSoundPlaying(true);
+          setTimerOn(true);
+          setTimeLeft(sessionLength);
+          setBreakOn(false);
+        }
       }
     }, 1000);
     return () => clearInterval(interval);
-  }, [timerOn, timeLeft]);
+  }, [timerOn, timeLeft, breakLength, breakOn, sessionLength]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = Math.floor(timeLeft % 60);
+  const timeString =
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
 
   return (
     <div className="flex flex-col items-center justify-center p-10">
@@ -89,20 +120,21 @@ function App() {
         </button>
       </div>
       <div>
-        <h2 id="timer-label">Session</h2>
-        <p id="time-left">
-          {new Intl.DateTimeFormat("en-GB", {
-            minute: "numeric",
-            second: "numeric",
-          }).format(timeLeft * 60 * 1000)}
-        </p>
+        <h2 id="timer-label">
+          {timerOn && <span>Session</span>}
+          {breakOn && <span>Break started</span>}
+        </h2>
+        <p id="time-left">{timeString}</p>
         <button id="start_stop" onClick={startStop}>
-          []
+          START/STOP
         </button>
         <button id="reset" onClick={reset}>
-          X
+          RESET
         </button>
       </div>
+      <audio id="beep" hidden>
+        <source src="beep.mp3" type="audio/mpeg"></source>
+      </audio>
     </div>
   );
 }
